44a3397743b43081f2b7881a906c55e168d96cdd
Serhii Semenykhin <semyonich@users.noreply.github.com>
2021-02-22 18:53:35 +0200

    one magic number removed

--- src/main/java/com/multithread/Application.java
+++ src/main/java/com/multithread/Application.java
@@ -2,9 +2,10 @@ package com.multithread;
 
 public class Application {
     private static final int ITERATIONS_NUMBER = 100;
+    private static final int COUNTER_START = 0;
 
     public static void main(String[] args) {
-        Counter counter = new Counter(0);
+        Counter counter = new Counter(COUNTER_START);
         RunnableClass runnable = new RunnableClass(ITERATIONS_NUMBER, counter);
         ThreadClass thread = new ThreadClass(ITERATIONS_NUMBER, counter);
         thread.start();
